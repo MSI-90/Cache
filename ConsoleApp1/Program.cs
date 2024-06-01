f7c619a3937b59748c56fbe5ba36252c87be2198
Sergey <s.miller@list.ru>
2024-06-01 15:13:34 +0700

    Program немного модифицирован

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,56 +4,59 @@ public class Program
 {
     static async Task Main(string[] args)
     {
-        var cach = new Cache<string>(15);
+        var cach = new Cache<string>(10);
         cach.ActionNotify += cach.Notify;
 
         var tokenSource = new CancellationTokenSource();
         var token = tokenSource.Token;
         //tokenSource.Cancel();
 
-        //await cach.GetOrAdd("ddddd", async() => "Hello", token);
-        //await cach.GetOrAdd("ddddd", async() => "Hello", token);
-        //await cach.GetOrAdd("ffff", async () => "World", token);
-        //await cach.GetOrAdd("555t5t", async () => "Сложная задача", token);
-        //cach.GetList();
+        await cach.GetOrAdd("ddddd", async () => "Hello", token);
+        await cach.GetOrAdd("ddddd", async () => "Hello", token);
+        await cach.GetOrAdd("ffff", async () => "World", token);
+        await cach.GetOrAdd("555t5t", async () => "Сложная задача", token);
+        cach.GetList();
 
-        //cach.Remove("ffff");
-        //cach.GetList();
-        //cach.GetTimesLifes();
+        cach.Remove("ffff");
+        cach.GetList();
+        cach.GetTimesLifes();
 
-        while (true)
-        {
-            Console.WriteLine("Введите ключ: ");
-            var keyboardValue = Console.ReadLine();
-            switch (keyboardValue)
-            {
-                case "exit":
-                    return;
 
-                case "list":
-                    await Console.Out.WriteLineAsync("Список всех элементов:");
-                    cach.GetList();
-                    break;
+        //Проверить через консоль)
 
-                case "life":
-                    await Console.Out.WriteLineAsync("Список всех элементов:");
-                    cach.GetTimesLifes();
-                    break;
+        //while (true)
+        //{
+        //    Console.WriteLine("Введите ключ: ");
+        //    var keyboardValue = Console.ReadLine();
+        //    switch (keyboardValue)
+        //    {
+        //        case "exit":
+        //            return;
 
-                case "remove":
-                    Console.WriteLine("Укажите ключ для удаления элемента: ");
-                    string? key = Console.ReadLine();
-                    cach.Remove(key);
-                    break;
-                
-                default:
-                    await cach.GetOrAdd(keyboardValue, MemoryFactory, token);
-                    break;
-            }
-        }
+        //        case "list":
+        //            await Console.Out.WriteLineAsync("Список всех элементов:");
+        //            cach.GetList();
+        //            break;
+
+        //        case "life":
+        //            await Console.Out.WriteLineAsync("Список всех элементов:");
+        //            cach.GetTimesLifes();
+        //            break;
+
+        //        case "remove":
+        //            Console.WriteLine("Укажите ключ для удаления элемента: ");
+        //            string? key = Console.ReadLine();
+        //            cach.Remove(key);
+        //            break;
+
+        //        default:
+        //            await cach.GetOrAdd(keyboardValue, MemoryFactory, token);
+        //            break;
+        //    }
+        //}
     }
-    static async Task<string> MemoryFactory()
-    {
-        return new string("feefefeefefef");
-    }
+    //static async Task<string> MemoryFactory()
+    //{
+    //    return new string("feefefeefefef");
+    //}
 }
